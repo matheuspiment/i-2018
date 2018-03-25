821ec03ca914bf90c632d585eef6d67bf6442af2
Matheus Ribeiro Pimenta Nunes <matheuspiment@hotmail.com>
2018-03-25 20:06:13 -0300

    Update Exercicio4.java

--- arquivos-java/src/arquivos/Exercicio4.java
+++ arquivos-java/src/arquivos/Exercicio4.java
@@ -8,10 +8,10 @@ public class Exercicio4 {
 	public static void main(String[] args) {
 
 	    try {
-				PrintWriter printWriter = new PrintWriter(args[0], "UTF-8");
-				
-				printWriter.write(args[1]);
-				printWriter.close();
+		PrintWriter printWriter = new PrintWriter(args[0], "UTF-8");
+
+		printWriter.write(args[1]);
+		printWriter.close();
 
 	    } catch (IOException e) {
 	        System.err.printf("Erro na abertura do arquivo: %s.\n",
