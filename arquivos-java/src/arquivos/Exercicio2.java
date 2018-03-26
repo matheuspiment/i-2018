f56c5bb7d1d7db00da477ac3c1c1e08dffb2b749
Matheus Ribeiro Pimenta Nunes <matheuspiment@hotmail.com>
2018-03-26 19:57:01 -0300

    Update Exercicio2.java

--- arquivos-java/src/arquivos/Exercicio2.java
+++ arquivos-java/src/arquivos/Exercicio2.java
@@ -8,27 +8,27 @@ public class Exercicio2	 {
 
 	public static void main(String[] args) {
 	    try {
-			File file = new File(args[0]);
-			RandomAccessFile randomAccess = new RandomAccessFile(file, "r");
-			byte[] byteArray = new byte[4];
+		File file = new File(args[0]);
+		RandomAccessFile randomAccess = new RandomAccessFile(file, "r");
+		byte[] byteArray = new byte[4];
 
-			randomAccess.seek(0);
-			randomAccess.read(byteArray, 0, 2);
+		randomAccess.seek(0);
+		randomAccess.read(byteArray, 0, 2);
 
-			randomAccess.seek(randomAccess.length() - 2);
-			randomAccess.read(byteArray, 2, 2);
+		randomAccess.seek(randomAccess.length() - 2);
+		randomAccess.read(byteArray, 2, 2);
 
-			String firstPair = Integer.toHexString(byteArray[0] & 0xFF) +
-				Integer.toHexString(byteArray[1] & 0xFF);
+		String firstPair = Integer.toHexString(byteArray[0] & 0xFF) +
+			Integer.toHexString(byteArray[1] & 0xFF);
 
-			String lastPair = Integer.toHexString(byteArray[2] & 0xFF) +
-				Integer.toHexString(byteArray[3] & 0xFF);
+		String lastPair = Integer.toHexString(byteArray[2] & 0xFF) +
+			Integer.toHexString(byteArray[3] & 0xFF);
 
-			if (firstPair.equals("ffd8") && lastPair.equals("ffd9")) {
-				System.out.println("É uma imagem jpg/jpeg");
-			} else {
-				System.out.println("Não é uma imagem jpg/jpeg");
-			}
+		if (firstPair.equals("ffd8") && lastPair.equals("ffd9")) {
+			System.out.println("É uma imagem jpg/jpeg");
+		} else {
+			System.out.println("Não é uma imagem jpg/jpeg");
+		}
 
 	    } catch (IOException e) {
 	        System.err.printf("Erro na abertura do arquivo: %s.\n",
