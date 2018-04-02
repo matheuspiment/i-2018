28e636e663bf7767eb1fb5a3e621f5df8d888d25
matheuspiment <matheuspiment@hotmail.com>
2018-04-02 20:19:29 -0300

    atualizar Exercicio6

--- arquivos-java/src/arquivos/Exercicio6.java
+++ arquivos-java/src/arquivos/Exercicio6.java
@@ -6,6 +6,7 @@ import java.io.BufferedReader;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.DataOutputStream;
+import java.nio.ByteBuffer;
 import java.io.IOException;
 
 public class Exercicio6 {
@@ -13,17 +14,19 @@ public class Exercicio6 {
 	public static void main(String[] args) {
 	    try {
 	    	FileInputStream file = new FileInputStream(args[0]);
-
 			DataInputStream dis = new DataInputStream(file);
-			int valor = dis.readInt();
-			// dis.close();
-			System.out.println(valor);
-			byte[] b = new byte[3];
-			dis.read(b);
-			String str = new String(b, "UTF-8");
-			System.out.println(str);
-			dis.close();
 
+			byte[] intByte = new byte[4];
+			int lineByteAmount;
+			while ((lineByteAmount = dis.read(intByte)) != -1) {
+				lineByteAmount = ByteBuffer.wrap(intByte).getInt();
+				byte[] byteArray = new byte[lineByteAmount];
+				dis.read(byteArray);
+				String str = new String(byteArray, "UTF-8");
+				System.out.println(str);
+			}
+
+			dis.close();
 
 	    } catch (IOException e) {
 	        System.err.printf("Erro na abertura do arquivo: %s.\n",
