e5e577140eefa5aa69bc30cd29eb9ee91b8f16b3
matheuspiment <matheuspiment@hotmail.com>
2018-04-16 20:40:27 -0300

    atualizar exercicio 7

--- arquivos/src/main/java/br/ufg/inf/es/integracao/apigithub/Exercicio7.java
+++ arquivos/src/main/java/br/ufg/inf/es/integracao/apigithub/Exercicio7.java
@@ -1,113 +1,60 @@
+package br.ufg.inf.es.integracao.apigithub;
+
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
-import java.net.URLEncoder;
-import com.google.gson.Gson;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 /**
- *
- * A complete Java class that shows how to open a URL, then read data (text) from that URL,
- * HttpURLConnection class (in combination with an InputStreamReader and BufferedReader).
- *
- * @author alvin alexander, http://alvinalexander.com.
- *
+ * Created by aluno on 16/04/18.
  */
-public class Exercicio7
-{
-  public static void main(String[] args)
-  throws Exception
-  {
-    new Exercicio7();
-  }
-  
-  public Exercicio7()
-  {
-    try
-    {
-      String myUrl = "https://api.github.com/users/kyriosdata";
-      // if your url can contain weird characters you will want to 
-      // encode it here, something like this:
-      // myUrl = URLEncoder.encode(myUrl, "UTF-8");
+public class Exercicio7 {
+  public static void main(String[] args) throws IOException {
+    String myUrl = args[0];
 
-      String results = doHttpUrlConnectionAction(myUrl);
-      System.out.println(results);
-    }
-    catch (Exception e)
-    {
-      // deal with the exception in your "controller"
-    }
-  }
-
-  /**
-   * Returns the output from the given URL.
-   * 
-   * I tried to hide some of the ugliness of the exception-handling
-   * in this method, and just return a high level Exception from here.
-   * Modify this behavior as desired.
-   * 
-   * @param desiredUrl
-   * @return
-   * @throws Exception
-   */
-  private String doHttpUrlConnectionAction(String desiredUrl)
-  throws Exception
-  {
-    URL url = null;
     BufferedReader reader = null;
-    StringBuilder stringBuilder;
 
-    try
-    {
-      // create the HttpURLConnection
-      url = new URL(desiredUrl);
+    try {
+
+      URL url = new URL(myUrl);
       HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-      
-      // just want to do an HTTP GET here
+
       connection.setRequestMethod("GET");
-      
-      // uncomment this if you want to write output to this url
-      //connection.setDoOutput(true);
-      
-      // give it 15 seconds to respond
       connection.setReadTimeout(15*1000);
       connection.connect();
 
-      // read the output from the server
       reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-      stringBuilder = new StringBuilder();
+      StringBuilder stringBuilder = new StringBuilder();
 
       String line = null;
-      while ((line = reader.readLine()) != null)
-      {
+      while ((line = reader.readLine()) != null) {
         stringBuilder.append(line + "\n");
       }
-      return stringBuilder.toString();
-    }
-    catch (Exception e)
-    {
+
+      String result = stringBuilder.toString();
+
+      JsonParser parser = new JsonParser();
+      JsonObject gitUserJSON = parser.parse(result).getAsJsonObject();
+      String avatarURL = gitUserJSON.get("avatar_url").toString().replaceAll("\"", "");
+      String userName = gitUserJSON.get("login").toString().replaceAll("\"", "");
+
+      System.out.println("URL do avatar " + avatarURL);
+
+      InputStream in = new URL(avatarURL).openStream();
+      Files.copy(in, Paths.get(userName+ ".jpg"));
+
+      System.out.println("Imagem salva no diretório arquivos com o nome " + userName + ".jpg");
+
+    } catch (Exception e) {
       e.printStackTrace();
-      throw e;
-    }
-    finally
-    {   
-        try(InputStream in = new URL("https://avatars1.githubusercontent.com/u/1735792?v=4").openStream()){
-        Files.copy(in, Paths.get("image.jpg"));
-        }
-      // close the reader; this can throw an exception too, so
-      // wrap it in another try/catch block.
-      if (reader != null)
-      {
-        try
-        {
-          reader.close();
-        }
-        catch (IOException ioe)
-        {
-          ioe.printStackTrace();
-        }
-      }
+    } finally {
+      reader.close();
     }
   }
 }
