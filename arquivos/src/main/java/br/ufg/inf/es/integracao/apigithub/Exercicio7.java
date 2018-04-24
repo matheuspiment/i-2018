2c95b6f8790ac347626a81cfda87d5ae1d51ef91
matheuspiment <matheuspiment@hotmail.com>
2018-04-23 21:30:07 -0300

    refatorar exercicio 7

--- arquivos/src/main/java/br/ufg/inf/es/integracao/apigithub/Exercicio7.java
+++ arquivos/src/main/java/br/ufg/inf/es/integracao/apigithub/Exercicio7.java
@@ -2,12 +2,11 @@ package br.ufg.inf.es.integracao.apigithub;
 
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
+
+import java.io.*;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 
@@ -17,53 +16,68 @@ import java.nio.file.Paths;
 public class Exercicio7 {
   public static void main(String[] args) throws IOException {
     String myUrl = args[0];
-
-    BufferedReader reader = null;
-
-    try {
-
-      URL url = new URL(myUrl);
-      HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-
-      connection.setRequestMethod("GET");
-      connection.setReadTimeout(15*1000);
-      connection.connect();
-
-      reader = getConnectionBuferredReader(connection);
-
-      String result = stringfy(reader);
-
-      JsonObject gitUserJSON = getAsJsonObject(result);
-
-      String avatarURL = getValueByKey(gitUserJSON, "avatar_url");
-      String userName = getValueByKey(gitUserJSON, "login");
-
-      System.out.println("URL do avatar " + avatarURL);
-
-      InputStream in = getAvatarInputStream(avatarURL);
-      Files.copy(in, Paths.get(userName+ ".jpg"));
-
-      System.out.println("Imagem salva no diretório arquivos com o nome " + userName + ".jpg");
-
-    } catch (Exception e) {
-      e.printStackTrace();
-    } finally {
-      reader.close();
-    }
+    new Exercicio7().getImageFromGitHubUserURL(myUrl);
   }
 
-  public static BufferedReader getConnectionBuferredReader(HttpURLConnection connection) throws IOException {
-      return new BufferedReader(new InputStreamReader(connection.getInputStream()));
-  }
+    public void getImageFromGitHubUserURL(String myUrl) {
+        try {
 
-  public static String stringfy(BufferedReader reader) throws IOException {
-    StringBuilder stringBuilder = new StringBuilder();
-    String line = null;
-    while ((line = reader.readLine()) != null) {
-        stringBuilder.append(line + "\n");
+          // Obtém URL do JSON
+            InputStream jsonIS = getInputStream(myUrl);
+
+          // Obtém o JSON
+          byte[] jsonByte = getByteArrayFromInputStream(jsonIS);
+          String json = new String(jsonByte, StandardCharsets.UTF_8);
+
+          // Extrai informações do JSON
+          JsonObject gitUserJSON = getAsJsonObject(json);
+
+          String avatarURL = getValueByKey(gitUserJSON, "avatar_url");
+          String userName = getValueByKey(gitUserJSON, "login");
+
+          InputStream in = getAvatarInputStream(avatarURL);
+
+          // Grava arquivo
+            gravaArquivo(userName, in);
+
+        } catch (Exception e) {
+          e.printStackTrace();
+        }
     }
 
-    return stringBuilder.toString();
+    public InputStream getInputStream(String myUrl) throws IOException {
+        URL url = new URL(myUrl);
+        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+
+        connection.setRequestMethod("GET");
+        connection.setReadTimeout(15*1000);
+        connection.connect();
+
+        return connection.getInputStream();
+    }
+
+    public void gravaArquivo(String userName, InputStream in) throws IOException {
+        Files.copy(in, Paths.get(userName+ ".jpg"));
+    }
+
+    public static byte[] getByteArrayFromInputStream(InputStream is) {
+      ByteArrayOutputStream buffer = new ByteArrayOutputStream();
+
+      int nRead;
+      byte[] data = new byte[16384];
+
+      try {
+
+          while ((nRead = is.read(data, 0, data.length)) != -1) {
+              buffer.write(data, 0, nRead);
+          }
+
+          buffer.flush();
+
+      } catch (Exception e) {
+          return null;
+      }
+      return buffer.toByteArray();
   }
 
   public static JsonObject getAsJsonObject(String jsonAsString) {
@@ -75,7 +89,7 @@ public class Exercicio7 {
       return parsedJsonObject.get(key).toString().replaceAll("\"", "");
   }
 
-  public static InputStream getAvatarInputStream(String avatarURL) throws IOException {
+  public InputStream getAvatarInputStream(String avatarURL) throws IOException {
     return new URL(avatarURL).openStream();
   }
 
