0c0085eeb1b542c41dd6a5a2fb039f0966992e94
matheuspiment <matheuspiment@hotmail.com>
2018-05-14 20:22:53 -0300

    improvements

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # i-2018
 
+[![Build Status](https://travis-ci.org/matheuspiment/i-2018.svg?branch=master)](https://travis-ci.org/matheuspiment/i-2018)
+
 Repositório das atividades da disciplina de Integração de Aplicações
--- topico-3/src/main/java/br/ufg/inf/es/integracao/Exercicio2.java
+++ topico-3/src/main/java/br/ufg/inf/es/integracao/Exercicio2.java
@@ -31,26 +31,26 @@ public class Exercicio2 {
      */
     public static void main(String[] args) throws IOException, XMLStreamException, JAXBException {
 
-//        File file = new File("calcados.xml");
-//        XmlMapper xmlMapper = new XmlMapper();
-//        String xml = inputStreamToString(new FileInputStream(file));
-//        Calcados value = xmlMapper.readValue(xml, Calcados.class);
-//        System.out.println(value.getCalcados().get(0).getMarca());
+        File file = new File("calcados.xml");
+        XmlMapper xmlMapper = new XmlMapper();
+        String xml = inputStreamToString(new FileInputStream(file));
+        Calcados value = xmlMapper.readValue(xml, Calcados.class);
+        System.out.println(value.getCalcados().get(0).getMarca());
 
-        Calcado a1 = new Calcado();
-        a1.setMarca("Nike");
-        a1.setPreco(142);
-        List<Calcado> calcados = new ArrayList<Calcado>();
-        calcados.add(a1);
-        calcados.add(a1);
-        Calcados c = new Calcados();
-        c.setCalcados(calcados);
-
-        JAXBContext context = JAXBContext.newInstance(Calcados.class);
-        Marshaller m = context.createMarshaller();
-        m.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, Boolean.TRUE);
-
-        m.marshal(c, System.out);
+//        Calcado a1 = new Calcado();
+//        a1.setMarca("Nike");
+//        a1.setPreco(142);
+//        List<Calcado> calcados = new ArrayList<Calcado>();
+//        calcados.add(a1);
+//        calcados.add(a1);
+//        Calcados c = new Calcados();
+//        c.setCalcados(calcados);
+//
+//        JAXBContext context = JAXBContext.newInstance(Calcados.class);
+//        Marshaller m = context.createMarshaller();
+//        m.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, Boolean.TRUE);
+//
+//        m.marshal(c, System.out);
     }
 
     public static String inputStreamToString(InputStream is) throws IOException {
